2f32b82a4264b6cd5de7d4de262db7e615aeae37
Seth Battis <sethbattis@stmarksschool.org>
2016-10-21 18:50:44 -0400

    Boy, cross-site scripting is tough!
    In fact, browsers don't let you do it. But… if you load a frame, it covers up a message that's behind the frame, so we'll settle for that being good enough.

---
+++ detect-iframe-fail.js
@@ -0,0 +1,23 @@
+var canvashack = {
+    checkIframes: function () {
+        "use strict";
+        var
+            frames = $('iframe:not([src*="instructure"]):not([id*="instructure"])'),
+            googleTest = /google\.com/;
+        if (frames.size() > 0) {
+            frames.wrap('<div class="canary"/>');
+            $('.canary').each(function (i, elt) {
+                var
+                    canary = $(elt),
+                    frame = $(elt).find('iframe');
+                canary.width(frame.width());
+                canary.height(frame.height());
+                frame.css('position', 'absolute').css('top', 0).css('left', 0).width('100%').height('100%');
+                canary.append('<p>Warning: IFRAME embed failed!</p>');
+                if (googleTest.test(frame.attr('src'))) {
+                    canary.append('<p>If you see nothing here, it&rsquo;s because you need to log in to Google. Open your Gmail in another tab, log in and then refresh this page.</p>');
+                }
+            });
+        }
+    }
+};
--- warning.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'common.inc.php';
-
-$smarty->prependTemplateDir(__DIR__ . '/templates');
-$smarty->assign([
-    'isGoogle' => preg_match('/google\.com.*ServiceLogin/', $_REQUEST['url'])
-]);
-$smarty->display('warning.tpl');
